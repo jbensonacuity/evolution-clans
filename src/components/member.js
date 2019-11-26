7b59d7a871fcc01a398ff96d6ba3a8b416f1e975
Damon <damon@damongolding.com>
2019-11-26 21:37:28 +0000

    Is platforn is undifined don't show "on"

--- src/components/member.js
+++ src/components/member.js
@@ -3,7 +3,7 @@ import Moment from 'moment';
 
 const Member = ({member, platform}) => {
 
-	const platforms = ["Unknown", "Xbox", "Ps4", "Steam", "Blizzard" ,"Stadia"];
+	const platforms = ["on Unknown", "on Xbox", "on Ps4", "on Steam", "on Blizzard" ,"on Stadia"];
 	const lastPlayed = Moment.unix(member.lastOnlineStatusChange).fromNow();
 	const today = Moment(new Date());
 	const daysSincePlayed = Moment.unix(member.lastOnlineStatusChange).diff(today, 'days');
@@ -16,7 +16,7 @@ const Member = ({member, platform}) => {
 				className={`member 
 					${(daysSincePlayed - daysSincePlayed * 2) > 89 ? "boot" : "dont-boot"} 
 					${member.memberType >= 3 ? "admin" : ""}`}>
-					<strong>{member.destinyUserInfo.displayName}</strong> : Played { lastPlayed } on {platforms[platform]}
+					<strong>{member.destinyUserInfo.displayName}</strong> : Played { lastPlayed } {platforms[platform]}
 			</div>
 		);
 	}
