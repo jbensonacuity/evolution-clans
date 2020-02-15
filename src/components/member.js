eb52bcc38c5da8790dcc79a50acf6b1a8f90ce0f
Joe Benson <joe@acuitydigital.com>
2020-02-14 18:28:28 -0600

    Add all cross saved profiles to member display

--- src/components/member.js
+++ src/components/member.js
@@ -4,11 +4,18 @@ import Moment from 'moment';
 const Member = ({member, platform}) => {
 
 	const platforms = ["on Unknown", "on Xbox", "on Ps4", "on Steam", "on Blizzard" ,"on Stadia"];
+	const xsavePlatforms = ["?", "XB1", "PSN", "Steam", "Bliz", "Stadia"];
 	const lastPlayed = Moment.unix(member.lastOnlineStatusChange).fromNow();
 	const today = Moment(new Date());
 	const daysSincePlayed = Moment.unix(member.lastOnlineStatusChange).diff(today, 'days');
 	//const membershipType = member.bungieNetUserInfo ? member.bungieNetUserInfo.membershipType : "nope";
 	//const membershipId = member.bungieNetUserInfo ? member.bungieNetUserInfo.membershipId : "nope";
+
+	//member.destinyUserInfo.applicableMembershipTypes - array of platforms in xsave
+	const xsavePlatformDisplay = member.destinyUserInfo.applicableMembershipTypes.map(function(xsavePlatform, index){
+		return index > 0 ? ", " + xsavePlatforms[xsavePlatform] : xsavePlatforms[xsavePlatform] ;
+	});
+
 
 	if (!member.destinyUserInfo && !member.bungieNetUserInfo)	{ 
 		return <div></div>; 
@@ -18,7 +25,7 @@ const Member = ({member, platform}) => {
 				className={`member 
 					${(daysSincePlayed - daysSincePlayed * 2) > 89 ? "boot" : "dont-boot"} 
 					${member.memberType >= 3 ? "admin" : ""}`}>
-						<strong>{member.destinyUserInfo.displayName}</strong> : Played { lastPlayed } {platforms[platform]}
+						<strong>{member.destinyUserInfo.LastSeenDisplayName}</strong> : Played { lastPlayed } {platforms[platform]} ({xsavePlatformDisplay})
 			</div>
 		);
 	}
