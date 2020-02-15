7d5174458bdcc40232f21b2d4bc7a7464d17aaca
Joe Benson <joe@acuitydigital.com>
2020-02-14 18:41:59 -0600

    cleaned xsave platform display logic, suppress entirely if only on a single platform

--- src/components/member.js
+++ src/components/member.js
@@ -4,17 +4,20 @@ import Moment from 'moment';
 const Member = ({member, platform}) => {
 
 	const platforms = ["on Unknown", "on Xbox", "on Ps4", "on Steam", "on Blizzard" ,"on Stadia"];
-	const xsavePlatforms = ["?", "XB1", "PSN", "Steam", "Bliz", "Stadia"];
+	const xsavePlatforms = ["?", "XB1", "PSN", "PC", "PC", "Stadia"];
 	const lastPlayed = Moment.unix(member.lastOnlineStatusChange).fromNow();
 	const today = Moment(new Date());
 	const daysSincePlayed = Moment.unix(member.lastOnlineStatusChange).diff(today, 'days');
 	//const membershipType = member.bungieNetUserInfo ? member.bungieNetUserInfo.membershipType : "nope";
 	//const membershipId = member.bungieNetUserInfo ? member.bungieNetUserInfo.membershipId : "nope";
 
-	//member.destinyUserInfo.applicableMembershipTypes - array of platforms in xsave
-	const xsavePlatformDisplay = member.destinyUserInfo.applicableMembershipTypes.map(function(xsavePlatform, index){
-		return index > 0 ? ", " + xsavePlatforms[xsavePlatform] : xsavePlatforms[xsavePlatform] ;
-	});
+	const xsavePlatformDisplay = member.destinyUserInfo.applicableMembershipTypes.length === 1 ? "" : 
+		"(" + 
+		member.destinyUserInfo.applicableMembershipTypes.map(
+			function(xsavePlatform, index) {
+				return xsavePlatforms[xsavePlatform] ;
+			})
+		+ ")";
 
 
 	if (!member.destinyUserInfo && !member.bungieNetUserInfo)	{ 
@@ -25,7 +28,7 @@ const Member = ({member, platform}) => {
 				className={`member 
 					${(daysSincePlayed - daysSincePlayed * 2) > 89 ? "boot" : "dont-boot"} 
 					${member.memberType >= 3 ? "admin" : ""}`}>
-						<strong>{member.destinyUserInfo.LastSeenDisplayName}</strong> : Played { lastPlayed } {platforms[platform]} ({xsavePlatformDisplay})
+						<strong>{member.destinyUserInfo.LastSeenDisplayName}</strong> : Played { lastPlayed } {platforms[platform]} {xsavePlatformDisplay}
 			</div>
 		);
 	}
